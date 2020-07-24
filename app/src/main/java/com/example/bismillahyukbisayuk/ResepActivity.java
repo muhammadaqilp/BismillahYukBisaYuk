89a580815bcf9359b1136902362a51bad305d1e9
muhammadaqilp <muhammadpratama1816@gmail.com>
2020-07-24 17:15:56 +0700

    menambahkan activity untuk menu resep makanan

---
+++ app/src/main/java/com/example/bismillahyukbisayuk/ResepActivity.java
@@ -0,0 +1,14 @@
+package com.example.bismillahyukbisayuk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resep);
+    }
+}
