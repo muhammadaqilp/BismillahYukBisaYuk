97230168eef8573fa7cd82ed85cb673719f6cedf
sabina165 <salsabilaramadhina615@gmail.com>
2020-08-06 12:12:40 +0700

    menambahkan ikon dan mengganti warna

--- app/src/main/java/com/example/bismillahyukbisayuk/LoginActivity.java
+++ app/src/main/java/com/example/bismillahyukbisayuk/LoginActivity.java
@@ -1,15 +1,14 @@
 package com.example.bismillahyukbisayuk;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -27,18 +26,18 @@ public class LoginActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         masuk = findViewById(R.id.btn_masuk);
-        daftarsekarang = findViewById(R.id.daftarsekarang);
+//        daftarsekarang = findViewById(R.id.daftarsekarang);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        daftarsekarang.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent daftar = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(daftar);
-            }
-        });
+//        daftarsekarang.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent daftar = new Intent(LoginActivity.this, RegisterActivity.class);
+//                startActivity(daftar);
+//            }
+//        });
 
         masuk.setOnClickListener(new View.OnClickListener() {
             @Override
