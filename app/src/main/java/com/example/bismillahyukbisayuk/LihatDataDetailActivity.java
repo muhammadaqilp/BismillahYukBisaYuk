ab2f7dd1c1a6eae3dd161c5c6c70a272e0d2bd2f
sabina165 <salsabilaramadhina615@gmail.com>
2020-08-15 14:45:33 +0700

    menambahkan layout yang diminta tapi belum masuk ke kodingan

---
+++ app/src/main/java/com/example/bismillahyukbisayuk/LihatDataDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.bismillahyukbisayuk;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LihatDataDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_data_detail);
+    }
+}
